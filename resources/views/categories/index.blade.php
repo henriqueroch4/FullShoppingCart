a1dba6f9f40e21733d3bba0e2f17285d1e0f7aba
Pedro Carvalho <equype1@gmail.com>
2019-08-10 16:27:16 -0300

    adicionando botao de excluir

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -6,15 +6,18 @@
 
 @section('conteudo')
     <div class="container">
-            <div class="jumbotron">
+            <div class="jumbotron mt-2">
                 <h1 class="display-4">Categorias</h1>
             </div>    
-            <ul>
+            <ul class="list-group">
                 @foreach ($categories as $category)
-                    <li>{{$category['name']}}</li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <span id="{{$category['id']}}">{{$category['name']}}</span>
+                        <button class="btn btn-danger btn-sm"><i class="material-icons">delete</i></button> 
+                    </li>
                 @endforeach
             </ul>
-            <a class="btn btn-primary text-white" href="{{route('cadastrar.categoria')}}">Cadastrar categoria</a>
+            <a class="btn btn-primary mt-2 float-right" href="{{route('cadastrar.categoria')}}">Cadastrar categoria</a>
     </div>
     
 @endsection
